40c4e24ca4e615ca713e26d0a4aa3f46c721c0b6
FelipeGabrill <felipegabrielodss@gmail.com>
2024-08-14 18:33:04 -0300

    Validação cpf (primeiro dígito e segundo dígito)

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,7 +1,10 @@
 export default function eUmCpf(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    validaNumeroRepetidos(cpf);
-    console.log(validaNumeroRepetidos(cpf));
+    if (validaNumeroRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log("Esse cpf não existe!");
+    } else {
+        console.log("Existe!")
+    }
 }
 
 function validaNumeroRepetidos(cpf) {
@@ -19,4 +22,40 @@ function validaNumeroRepetidos(cpf) {
     ];
 
     return numeroRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
 }
