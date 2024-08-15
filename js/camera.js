2ffd65f56fe5004c6f231198f1d38db863146752
FelipeGabrill <felipegabrielodss@gmail.com>
2024-08-14 21:51:56 -0300

    Capturando a imagem

--- js/camera.js
+++ js/camera.js
@@ -1,6 +1,10 @@
 const botaoIniciarCamera = document.querySelector("[data-video-botao]");
 const campoCamera = document.querySelector("[data-camera]");
 const video = document.querySelector("[data-video]");
+const botaoTirarFoto = document.querySelector("[data-tirar-foto]");
+const canvas = document.querySelector("[data-video-canvas]");
+const mensagem = document.querySelector("[data-mensagem]");
+let imagemURL = "";
 
 botaoIniciarCamera.addEventListener("click", async function() {
     const iniciarVideo = await navigator.mediaDevices
@@ -10,4 +14,11 @@ botaoIniciarCamera.addEventListener("click", async function() {
     campoCamera.style.display = "block";
 
     video.srcObject = iniciarVideo;
+});
+
+botaoTirarFoto.addEventListener("click", function() {
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.clientWidth, canvas.height);
+
+    imagemURL = canvas.toDataURL("image/jpeg");
+    mensagem.style.display = "block";
 });
